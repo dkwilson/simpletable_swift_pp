e3a453a1e6a7b323fa520cff6aa551ac279a77d6
dkwilson <general@ytechnology.net>
2022-01-04 23:02:17 -0500

    Initial Commit

---
+++ simpletable/simpletableApp.swift
@@ -0,0 +1,17 @@
+//
+//  simpletableApp.swift
+//  simpletable
+//
+//  Created by Dkeith Wilson on 1/4/22.
+//
+
+import SwiftUI
+
+@main
+struct simpletableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
