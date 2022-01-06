20874204b2ceb454e5f852d77c8a8c5bac095e52
dkwilson <general@ytechnology.net>
2022-01-05 21:32:16 -0500

    updated loop to fix list views

--- simpletable/ContentView.swift
+++ simpletable/ContentView.swift
@@ -8,9 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Resturant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Gram Avenue Meats and Deli", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrfina", "Donostia", "Royal Oak", "CASK Pub and Kitchen"
+    
+    ]
+    
+    var restaurantImages = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats And Deli", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrfina", "Donostia", "Royal Oak", "CASK Pub and Kitchen"]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        List {
+            ForEach(restaurantNames.indices, id: \.self) { index in
+                HStack {
+                    Image(restaurantImages[index])
+                        .resizable()
+                        .frame(width: 40, height: 40)
+                        
+                    Text(restaurantNames[index])
+                }
+                    
+            }
+        }
+        .listStyle(.plain)
     }
 }
 
